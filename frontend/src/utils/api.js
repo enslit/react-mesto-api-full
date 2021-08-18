23d5159bb2a10f9e257907b1073581a0952b4c8b
Aleksandr <alex@enslit.ru>
2021-08-18 23:28:52 +0300

    change api url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,14 +2,14 @@ import { AppApi } from './classes/AppApi';
 import { AuthApi } from './classes/AuthApi';
 
 export const api = new AppApi({
-  baseUrl: 'https://api.enslit.nomoredomains.monster/api',
+  baseUrl: 'https://mesto.enslit.ru/api',
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
 export const auth = new AuthApi({
-  baseUrl: 'https://api.enslit.nomoredomains.monster/api',
+  baseUrl: 'https://mesto.enslit.ru/api',
   headers: {
     'Content-Type': 'application/json',
   },
